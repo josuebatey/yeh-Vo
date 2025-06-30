b8cce9f0de6aa5a5b2d864e5690521d147d41aa4
Josue Batey <josue.batey0@gmail.com>
2025-06-30 22:48:32 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ function AppRoutes() {
   if (!user) {
     return (
       <>
-        <AuthPage className='border' />
+        <AuthPage className='border border-red-600'  />
         <span className="object-fill flex justify-end fixed bottom-2 right-2 z-index-9999">
           <img className="h-10 w-20 mt-2" src="../../public/bolt-bagde.svg" />
       </span>
