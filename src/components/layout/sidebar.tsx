f7c968ac84376e2cd9e35870f16cf34ef8b19138
Josue Batey <josue.batey0@gmail.com>
2025-06-30 22:30:36 +0200

    Updated sidebar.tsx

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -101,8 +101,8 @@ export function Sidebar({ collapsed }: SidebarProps) {
           collapsed && "hidden"
         )}>
           Built with ❤️ <BoltBadge className='border border-red-400' />
-          <span className="h-10 w-20 object-fill border border-white">
-            <img className="h-fit " src="../../public/bolt-bagde.svg" />
+          <span className="object-fill border border-white">
+            <img className="h-10 w-20" src="../../public/bolt-bagde.svg" />
           </span>
         </p>
       </div>
