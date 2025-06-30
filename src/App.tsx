e96f6682256ac7a439ce34872603e82cc89a72bf
Josue Batey <josue.batey0@gmail.com>
2025-06-30 22:40:32 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -81,7 +81,7 @@ function AppRoutes() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Layout>
-      <span className="object-fill flex justify-end border border-red-500 fixed">
+      <span className="object-fill flex justify-end border border-red-500 fixed top-2 right-2">
           <img className="h-10 w-20 mt-2" src="../../public/bolt-bagde.svg" />
       </span>
     </>
